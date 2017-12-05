385d010854c8fe16c51f033004cf069bd6da3134
Cédric Caspar <cedric.caspar@gmail.com>
2017-12-06 00:12:08 +0100

    Let in guid class

--- src/utilities/guid.js
+++ src/utilities/guid.js
@@ -1,23 +1,22 @@
-export class Guid{
-    
-    internalGuid = '';
+export class Guid {
+  internalGuid = '';
+  constructor() {
+    this.setRandomInternalGuid();
+  }
 
-    constructor(){
-        this.setRandomInternalGuid();
-    }
+  toString() {
+    return this.internalGuid;
+  }
 
-    toString(){
-        return this.internalGuid;
-    }
+  static newGuid() {
+    return new Guid();
+  }
 
-    static newGuid(){
-        return new Guid();
-    }
-
-    setRandomInternalGuid(){
-        this.internalGuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-          });
-    }
+  setRandomInternalGuid() {
+    this.internalGuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      let r = Math.random() * 16 | 0,
+          v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 }
